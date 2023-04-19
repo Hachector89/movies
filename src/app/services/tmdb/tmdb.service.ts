2c8b1820207dbc45702d7f892a095f4a2260759a
Hector <hectormoreno1989@hotmail.com>
2023-04-19 19:13:56 +0200

    servicio endpoints

--- src/app/services/tmdb/tmdb.service.ts
+++ src/app/services/tmdb/tmdb.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ConfigService } from '../configService/config.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TmdbService {
 
-  constructor() { }
+  readonly apiKey: string = this.config.get('apiKey');
+
+  readonly baseUrl: string = 'https://api.themoviedb.org/3/';
+
+  constructor(
+    private config: ConfigService,
+    private http: HttpClient
+  ) { }
+
+  getMovieGenres(): Observable<any> {
+    const params = new HttpParams().set('api_key', this.apiKey);
+    return this.http.get(`${this.baseUrl}genre/movie/list`, { params });
+  }
+
+  getMoviesThisWeek(): Observable<any> {
+    const params = new HttpParams().set('api_key', this.apiKey);
+    return this.http.get(`${this.baseUrl}movie/now_playing`, { params });
+  }
+
+  getMovieDetails(movieId: number): Observable<any> {
+    const params = new HttpParams().set('api_key', this.apiKey);
+    return this.http.get(`${this.baseUrl}movie/${movieId}`, { params });
+  }
+
 }
